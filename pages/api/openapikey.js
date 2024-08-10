3ca3f5968ecc4a7752837d96b46d62478cc345bd
seunghyonpark <creath.park@gmail.com>
2024-08-10 20:34:17 +0900

    dsfsdafds

--- pages/api/openapikey.js
+++ pages/api/openapikey.js
@@ -19,6 +19,12 @@ export default async function handler(req, res) {
   const openapikey = process.env.OPENAI_API_KEY;
 
 
+  console.log("openapikey: " + openapikey);
+
+
+  
+
+
   res.status(200).json(openapikey);
 
 
