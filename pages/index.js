26227a0d6e1ec68c6e1750055100fa0d1ec7bdc6
seunghyonpark <creath.park@gmail.com>
2024-08-12 11:36:30 +0900

    sadfsdfsdaf

--- pages/index.js
+++ pages/index.js
@@ -256,7 +256,7 @@ export default function Home() {
                 <img
                   className={styles.imgPreview}
                   src={result.url}
-                  onClick={() => download(result.url)}
+                  //onClick={() => download(result.url)}
                 />
               </div>
             );
