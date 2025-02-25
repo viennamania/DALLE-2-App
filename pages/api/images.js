4288ac9bef5e638e525353c05ce46f0b1a234a3a
seunghyonpark <creath.park@gmail.com>
2025-02-25 17:07:46 +0900

    Fix commented-out code to return a 405 error response in images API

--- pages/api/images.js
+++ pages/api/images.js
@@ -63,7 +63,7 @@ export default async function handler(req, res) {
 
   // return error
 
-  ///return res.status(405).json({ message: 'Method Not Allowed' });
+  return res.status(405).json({ message: 'Method Not Allowed' });
   
 
  
